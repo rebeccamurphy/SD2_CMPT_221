398237bbe59fbe04441f8f93975a845510df978b
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2015-02-11 17:14:40 -0500

    created index.php

---
+++ project1/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+<html>
+	<title>Project 1 </title>
+	<head>
+		<p>
+			Housing Recommendation Form 
+		</p>
+	</head>
+	<body>
+			
+	<form action="demo_form.asp">
+		<label for="name">Name</label>
+		<input type="text" name="name">
+		<br>
+		<br>
+		<label for="cwid">CWID</label>
+		<input type="text" name="cwid">
+		<br>
+		<br>
+		<label for="male">Male</label>
+		<input type="radio" name="sex" id="male" value="male">
+		<br>
+		<br>
+		<label for="female">Female</label>
+		<input type="radio" name="sex" id="female" value="female">
+		<br>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+	</body>
+</html>
